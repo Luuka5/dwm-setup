d3b9ee07ff15fa0492704e606c28ea17c0f47b90
Luuka5 <touko.paivansalo@gmail.com>
2023-10-21 22:42:45 +0300

    Add vscode theme change script

--- vscodetheme.sh
+++ vscodetheme.sh
@@ -1,3 +1,3 @@
-theme="Community Material Theme Darker High Contrast"
+theme="Dark Modern"
 
 sudo sed -i -e 's/"workbench.colorTheme": ".*"/"workbench.colorTheme": "$theme"/g' "$HOME/.config/Code/User/settings.json"
