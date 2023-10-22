6a61ce1d11c23c096c2602da29ba3aa69115ed51
Touko Päivänsalo <touko.paivansalo@gmail.com>
2023-10-22 18:58:50 +0300

    Add device.diff file and fix a bug

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 
 # install useful apps with yay
-yay -Syu neovim picom dmenu alacritty firefox xorg
+yay -Syu neovim picom dmenu alacritty firefox xorg feh
 
 # install dwm
 yay -S base-devel libx11 libxft libxinerama freetype2 fontconfig
+git checkout 6.4
 git clone https://git.suckless.org/dwm
 cd dwm
 git apply ../dwm-masterpatch-6.4.diff
