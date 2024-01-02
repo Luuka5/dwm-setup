ba6ea34659697aeb9ee31adbb1183d851b755eee
Touko Päivänsalo <touko.paivansalo@gmail.com>
2024-01-02 22:08:43 +0200

    Add dedicated restart dwm shortcut MODKEY+r

--- config.h
+++ config.h
@@ -78,12 +78,17 @@ static const char *dmenucmd[] = {
     "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
     "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
 static const char *termcmd[] = {"alacritty", NULL};
+static const char *browsercmd[] = {"firefox", NULL};
+static const char *restartdwmcmd[] = {"killall", "dwm"};
 
 static const Key keys[] = {
     /* modifier                     key        function        argument */
     {MODKEY, XK_p, spawn, {.v = dmenucmd}},
     {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
+    {MODKEY, XK_q, spawn, {.v = browsercmd}},
+    {MODKEY | ShiftMask, XK_q, spawn, {.v = restartdwmcmd}},
     {MODKEY, XK_b, togglebar, {0}},
+    {MODKEY, XK_a, togglebar, {0}},
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
     {MODKEY, XK_i, incnmaster, {.i = +1}},
@@ -112,7 +117,7 @@ static const Key keys[] = {
     {MODKEY, XK_g, setgaps, {.i = GAP_TOGGLE}},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+            TAGKEYS(XK_9, 8){MODKEY, XK_r, quit, {0}},
 };
 
 /* button definitions */
